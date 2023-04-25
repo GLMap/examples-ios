1092b27d6bf45544e8539b8ce804a3ec40311e07
Evgen Bodunov <evgen@getyourmap.com>
2023-04-25 18:53:41 +0200

    SwiftUI tests

---
+++ SwiftUIDemo/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by Evgen Bodunov on 25.04.23.
+//  Copyright © 2023 Evgen Bodunov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Evgen Bodunov on 25.04.23.
+//  Copyright © 2023 Evgen Bodunov. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
